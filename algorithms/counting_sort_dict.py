08ebfcac84e380b152aa382fd22c34a02b89cdfa
travisjungroth <jungroth@gmail.com>
2021-04-20 09:17:46 -0500

    Rename variable

--- algorithms/counting_sort_dict.py
+++ algorithms/counting_sort_dict.py
@@ -8,5 +8,5 @@ def counting_sort_dict(items: Iterable[T], key: Callable[[T], int] = lambda x: x
     groups = defaultdict(list)
     for item in items:
         groups[key(item)].append(item)
-    for i in range(min(groups), max(groups) + 1):
-        yield from groups[i]
+    for k in range(min(groups), max(groups) + 1):
+        yield from groups[k]
