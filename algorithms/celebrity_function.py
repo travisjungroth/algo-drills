1f11d183725e528f58fb61e4e3654f1440f4a0ff
travisjungroth <jungroth@gmail.com>
2021-04-20 07:45:47 -0500

    Refactor variable name

--- algorithms/celebrity_function.py
+++ algorithms/celebrity_function.py
@@ -5,11 +5,11 @@ from typing import Optional
 
 def celebrity_function(knows: Callable[[int, int], bool], n: int) -> Optional[int]:
     candidate = 0
-    for i in range(1, n):
-        if knows(candidate, i):
-            candidate = i
-    if any(knows(candidate, i) for i in range(candidate)):
+    for node in range(1, n):
+        if knows(candidate, node):
+            candidate = node
+    if any(knows(candidate, node) for node in range(candidate)):
         return None
-    if any(not knows(i, candidate) for i in range(n)):
+    if any(not knows(node, candidate) for node in range(n)):
         return None
     return candidate
