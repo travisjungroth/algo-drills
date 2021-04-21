e3ee57757638455ad4eb33c578a08aea392405bd
travisjungroth <jungroth@gmail.com>
2021-04-21 18:14:42 -0500

    Add docstring

--- algorithms/backpedal.py
+++ algorithms/backpedal.py
@@ -1,5 +1,8 @@
 """
 ID: ebd60a5b-403f-4eac-a651-8a9a52c2b11c
+
+Useful with Dijkstra's when you want the path. This version assumes all the keys you need are there,
+but you could use .get() instead.
 """
 from collections.abc import Mapping
 from typing import Optional
@@ -8,6 +11,7 @@ from hints import Node
 
 
 def backpedal(goal: Node, parents: Mapping[Node, Optional[Node]]) -> list[Node]:
+    """With a dict of parent->child, walk backwards from the goal as far as possible and return the path."""
     back_path = []
     node = goal
     while node is not None:
