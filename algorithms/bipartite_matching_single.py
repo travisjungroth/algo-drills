0f7dbcf4a88c3b4c05fcbbabc60a273b76163dce
travisjungroth <jungroth@gmail.com>
2021-04-21 20:22:09 -0500

    Comments and variables

--- algorithms/bipartite_matching_single.py
+++ algorithms/bipartite_matching_single.py
@@ -8,8 +8,8 @@ from collections.abc import Sequence
 def bipartite_matching_single(graph: Sequence[int]) -> list[int]:
     matches = list(graph)
     in_degrees = [0] * len(graph)  # This is an alternate to collections.Counter when the keys are 0-n.
-    for node in graph:
-        in_degrees[node] += 1
+    for target in graph:
+        in_degrees[target] += 1
     zero_in_degrees = [node for node, count in enumerate(in_degrees) if not count]
     while zero_in_degrees:
         node = zero_in_degrees.pop()
