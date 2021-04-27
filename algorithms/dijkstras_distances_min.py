2246b71cf76f078f6f0336476e4f04aa33e0f1f6
travisjungroth <jungroth@gmail.com>
2021-04-27 15:08:49 -0500

    Update docs

--- algorithms/dijkstras_distances_min.py
+++ algorithms/dijkstras_distances_min.py
@@ -1,7 +1,11 @@
 """
 ID: 1288e78d-bc7a-44da-8774-41c0e4dcfe70
+Grokking Algorithms, Page 115
 Python Algorithms, Page 194
 https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
+The explanation in Grokking Algorithms is good, but the code is needlessly complicated.
+This is a minimalist Dijkstras. See the other versions for more features and better time complexity.
 """
 from collections.abc import Mapping
 from math import inf
