bd007a478e4968cecec28058395a220ae31bbfaa
travisjungroth <jungroth@gmail.com>
2021-04-28 13:00:31 -0500

    Make practice a command

--- src/commands.py
+++ src/commands.py
@@ -1,32 +1,24 @@
-#!/usr/bin/env python3
-"""Optionally takes the uuid or name of the algorithm you want."""
 from datetime import timedelta
-import sys
 
 from src import Algo, Completion, minutes_seconds, start_timer, stop_timer, Workspace
 
 
-def practice() -> None:
+def practice(args) -> str:
+    out: list[str] = []
     workspace = Workspace()
     algo = workspace.algo()
     if workspace.matches(algo):
         Completion(algo).append_to_file()
         completion_time = stop_timer()
         if completion_time < timedelta(minutes=15):
-            print(minutes_seconds(completion_time))
+            out.append(minutes_seconds(completion_time))
         if Algo.done_today().issuperset(Algo.allowed()):
-            print('Did every algo today')
+            out.append('Did every algo today')
     if algo is None or workspace.matches(algo):
-        if len(sys.argv) > 1:
-            requested_algo = Algo.from_id_or_name(sys.argv[1])
-        else:
-            requested_algo = None
+        requested_algo = Algo.from_id_or_name(args.algo)
         new_algo = workspace.advance(requested_algo)
         start_timer()
-        print(f'Try {new_algo}')
+        out.append(f'Try {new_algo}')
     else:
-        print(algo.diff(workspace))
-
-
-if __name__ == '__main__':
-    practice()
+        out.append(algo.diff(workspace))
+    return '\n'.join(out)
