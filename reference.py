4ee75eafd4c9361149f6664343dae674b9c22a2d
travisjungroth <jungroth@gmail.com>
2021-04-28 09:46:09 -0500

    Fix reference sorting

--- reference.py
+++ reference.py
@@ -6,14 +6,14 @@ import sys
 from src import Algo
 
 ref = sys.argv[1]
-x = []
+pages_and_algos = []
 allowed = Algo.allowed()
 for algo in Algo.all():
     if algo in allowed:
         continue
     m = re.search(fr'{ref}, Page (\d+)', algo.text())
     if m is not None:
-        x.append((m.group(1), algo))
-x.sort()
-for page, algo in x:
+        pages_and_algos.append((m.group(1), algo))
+pages_and_algos.sort(key=lambda x: (int(x[0]), x[1].name))
+for page, algo in pages_and_algos:
     print(f'{algo}, Page {page}')
