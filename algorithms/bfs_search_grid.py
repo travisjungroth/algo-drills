3d0cd103660159cf806b12248682dd3bef79c96f
travisjungroth <jungroth@gmail.com>
2021-05-19 09:43:09 -0500

    Only add valid nodes to seen

--- algorithms/bfs_search_grid.py
+++ algorithms/bfs_search_grid.py
@@ -17,8 +17,8 @@ def bfs_search_grid(grid: Sequence[Sequence[int]], start: tuple[int, int], goal:
             return True
         adjacent = {(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)}
         for next_node in adjacent - seen:
-            seen.add(next_node)
             r1, c1 = next_node
             if r1 in rows and c1 in cols and grid[r1][c1]:
+                seen.add(next_node)
                 to_visit.append(next_node)
     return False
