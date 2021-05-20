3e3e447a4efe4f591ed6202478a522250c736f90
marsjoy <marsycielo@gmail.com>
2021-05-20 12:34:33 -0500

    one off error

--- algorithms/josephus_problem_iter.py
+++ algorithms/josephus_problem_iter.py
@@ -6,6 +6,6 @@ https://leetcode.com/problems/find-the-winner-of-the-circular-game/
 
 def josephus_problem_iter(n: int, k: int) -> int:
     survivor = 0
-    for i in range(2, n + 2):
+    for i in range(2, n + 1):
         survivor = (survivor + k) % i
     return survivor + 1
