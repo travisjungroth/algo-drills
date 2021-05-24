26408425df13a0cb0c0117834524e9271fc00017
travisjungroth <jungroth@gmail.com>
2021-05-24 09:28:33 -0500

    Rename variables

--- algorithms/prims.py
+++ algorithms/prims.py
@@ -14,12 +14,12 @@ from src.typehints import Node
 def prims(graph: Mapping[Node, Mapping[Node, int]], start: Node) -> dict[Node, Optional[Node]]:
     """Find the minimum spanning tree of an undirected, weighted graph."""
     parents = {}
-    pq = [(0, None, start)]  # (weight, parent, node)
-    while pq:
-        _, parent, node = heappop(pq)
+    to_visit = [(0, None, start)]
+    while to_visit:
+        weight, parent, node = heappop(to_visit)
         if node in parents:
             continue
         parents[node] = parent
         for next_node, weight in graph[node].items():
-            heappush(pq, (weight, node, next_node))
+            heappush(to_visit, (weight, node, next_node))
     return parents
